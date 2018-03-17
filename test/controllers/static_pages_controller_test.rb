588ff65d05e9a4a5014243705a7ac386f79e75fa
scrtree <M.kinoshita@gmail.com>
2018-03-17 11:42:39 +0900

    [add] 5.3.2 演習3

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -11,7 +11,7 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
     assert_select "title", "Ruby on Rails Tutorial Sample App"  end
 
   test "should get help" do
-    get helf_path
+    get help_path
     assert_response :success
     assert_select "title", "Help | #{@base_title}"
   end
