143b926ffbd0e20fd6981226b1452a23673500d9
scrtree <M.kinoshita@gmail.com>
2018-03-21 22:43:10 +0900

    [add] 6.2.5 一意性を検証する

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180310072902) do
+ActiveRecord::Schema.define(version: 20180321133811) do
 
   create_table "microposts", force: :cascade do |t|
     t.text "content"
@@ -24,6 +24,7 @@ ActiveRecord::Schema.define(version: 20180310072902) do
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
 end
