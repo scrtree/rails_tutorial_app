afd6b47c40bd7ed338ff15a4b51e1f5e5557734f
scrtree <M.kinoshita@gmail.com>
2018-04-07 10:41:47 +0900

    [add] 8.2 ログイン

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,4 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-  def hello
-    render html: "¡hola, mundo!"
-  end
-  def goodbye
-    render html: "goodbye, world!"
-  end  
+  include SessionsHelper
 end
