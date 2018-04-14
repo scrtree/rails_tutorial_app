8c1643cf2fa3ec0b1a23cc1d4ae4e52ea2f7b348
scrtree <M.kinoshita@gmail.com>
2018-04-14 23:03:20 +0900

    [add] 10.4.1 管理ユーザー 演習

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -10,7 +10,7 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
     get users_path
     assert_redirected_to login_url
   end
-  
+
   test "should get new" do
     get signup_path
     assert_response :success
@@ -27,6 +27,16 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
                                               email: @user.email } }
     assert_not flash.empty?
     assert_redirected_to login_url
+  end
+
+  test "should not allow the admin attribute to be edited via the web" do
+    log_in_as(@other_user)
+    assert_not @other_user.admin?
+    patch user_path(@other_user), params: {
+                                    user: { password:              @other_user.password,
+                                            password_confirmation: @other_user.password_confirmation,
+                                            admin: true } }
+    assert_not @other_user.reload.admin?
   end
 
   test "should redirect edit when logged in as wrong user" do
